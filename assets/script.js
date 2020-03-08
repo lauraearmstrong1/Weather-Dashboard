d19ffa95fc31fdb8eb0af2472ec7feeffa2d6dab
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-03-08 19:55:01 -0400

    updated with five day

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,22 @@
 $(document).ready(function () {
 
-    $(".list").on("click",".list-group-item", function(){
-       var cityClicked = $(this).attr("data-name")
-       current(cityClicked ,"old")
+    $(".list").on("click", ".list-group-item", function () { //when search btn is clicked then the city name is added to the list
+        var cityClicked = $(this).attr("data-name")
+        var cityID = $(this).attr("data-id")
+        current(cityClicked, "old")
     })
     function cityInfo() {
 
         var city = JSON.parse(localStorage.getItem("weather"))
-   
-       
         var theList = $("<ul>").addClass("list-group")
-        if(city){
+        if (city) {
 
             for (var i = 0; i < city.length; i++) {
                 var nameDiv = $('<li>').addClass("list-group-item")
                 nameDiv.text(city[i])
                 nameDiv.attr("data-name", city[i])
                 theList.prepend(nameDiv)
-    
+
             }
             $(".list").html("")
             $(".list").append(theList)
@@ -25,10 +24,34 @@ $(document).ready(function () {
     }
     $("#searchBTN").click(function () {
         var cityToSearch = $("#search").val()
+        var cityID = $("#search").val()
         current(cityToSearch, "new")
+        fiveDay(cityID)
     })
 
-    //function fiveDay(cityID)
+    function fiveDay(cityID) {
+        var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=c55054d69fe933e8a6f2b946136a8302&units=imperial"
+        $.get(fiveDayURL).then(function (response) {
+            var title = $("<h3>").text("5-Day Forecast")
+            $("#info").append(title)
+            for (var i = 0; i < response.list.length; i++) {
+                var icon = $("<img src='http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png'>")
+                var current = moment(response.list[i].dt_txt)
+                if (response.list[i].dt_txt.includes("15:00:00")) {
+                    var day = $("<div>").addClass("card col-sm-2 ml-1 float-left bg-primary")
+                    $("#info").append(day)
+                    var date = $('<div>').text(current.format('l'))
+                    var temp = $('<div>').text("Temp: " + response.list[i].main.temp + " \xB0F")
+                    var humidity = $("<div>").text("Humidity: " + response.list[i].main.humidity + "%")
+                    day.append(date)
+                    day.append(icon)
+                    day.append(temp)
+                    day.append(humidity)
+                }
+
+            }
+        })
+    }
 
     function current(cityToSearch, age) {
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityToSearch + "&appid=c55054d69fe933e8a6f2b946136a8302&units=imperial"
@@ -38,17 +61,20 @@ $(document).ready(function () {
             var $h2 = $("<h2>").text(results.name + " (" + now + ")");
             $h2.text(results.name + " (" + now + ")")
             var icon = $("<img src='http://openweathermap.org/img/wn/" + results.weather[0].icon + "@2x.png'>")
+            var uv = $()
             $h2.append(icon)
-            var temp = $("<div>").text("Temperature: " + results.main.temp + " F")
+            var temp = $("<div>").text("Temperature: " + results.main.temp + " \xB0F")
             var humidity = $("<div>").text("Humidity: " + results.main.humidity + "%")
             var windSpeed = $("<div>").text("Wind Speed: " + results.wind.speed + " MPH")
+            console.log(results);
+            fiveDay(results.id)
             currentWeather.append($h2)
             currentWeather.append(temp)
             currentWeather.append(humidity)
             currentWeather.append(windSpeed)
             $("#info").text("") //empties the previous current weather
             $("#info").append(currentWeather)
-            if(age ==="new"){
+            if (age === "new") {
                 var local = JSON.parse(localStorage.getItem("weather"))
                 if (!local) {
                     local = []
@@ -59,7 +85,6 @@ $(document).ready(function () {
                 local.push(results.name)
                 localStorage.setItem("weather", JSON.stringify(local))
                 cityInfo()
-                //fiveDay(cityID)
             }
         })
     }
